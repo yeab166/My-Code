228e0103966e0d84b53d6f3fee0265041c5968ae
Yeabsira Belete <yeababa16@gmail.com>
2024-12-13 13:17:26 +0300

    Update number of digits.cpp

--- Yeabsira_Belete/playing with numbers/number of digits.cpp
+++ Yeabsira_Belete/playing with numbers/number of digits.cpp
@@ -1,1 +1,23 @@
-a
+#include <iostream>
+#include <cmath>
+using namespace std;
+ int main () { 
+   int num,remainder,sum = 0;
+      cout << " num: ";
+      cin >> num;
+         libel:
+            remainder = num % 10;
+              if ( remainder >= 0 )
+                 {
+                  sum += 1;
+                  
+                  num /= 10;
+                    if ( num > 0 )
+                      {
+                        goto libel;
+                      }
+                 }
+                    cout << " sum of the digit is: " << sum;
+     
+ return 0;
+}
